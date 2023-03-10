9d68a2fd91c223baca959dcbee6c658819aa8a0d
Imene KOLLI <53266529+imene-swaan@users.noreply.github.com>
2023-03-10 16:43:35 +0100

    Delete src directory

--- src/main.py
+++
@@ -1,8 +0,0 @@
-
-from pdf2image import convert_from_path
-
-if __name__ == '__main__':
-    for i in range(1, 6):
-        pages = convert_from_path(f'../data/Document {i}.pdf', 500)
-        for page in pages:
-            page.save(f'document_{i}.png', 'PNG')
